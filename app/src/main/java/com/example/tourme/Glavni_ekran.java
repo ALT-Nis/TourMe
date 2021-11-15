2c1cf4862e1b1eaf4e2262957934af0129a3242b
AsterixMitic <58214631+AsterixMitic@users.noreply.github.com>
2021-11-15 20:00:45 +0100

    Dodat splashscreen, logo, glavni ekran

---
+++ app/src/main/java/com/example/tourme/Glavni_ekran.java
@@ -0,0 +1,14 @@
+package com.example.tourme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Glavni_ekran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glavni_ekran);
+    }
+}
