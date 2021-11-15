06869b4c307e50649428ac68027658eb731feed7
AsterixMitic <58214631+AsterixMitic@users.noreply.github.com>
2021-11-15 20:27:46 +0100

    Dodat login aktiviti

--- app/src/main/java/com/example/tourme/Glavni_ekran.java
+++ app/src/main/java/com/example/tourme/Glavni_ekran.java
@@ -2,7 +2,10 @@ package com.example.tourme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Glavni_ekran extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Glavni_ekran extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glavni_ekran);
+
+        Button dugmeLogin = findViewById(R.id.dugme_za_login);
+        dugmeLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Login.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
