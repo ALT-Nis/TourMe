f37fd6f27b7adc81082f63a404a1bb32537bb956
AsterixMitic <58214631+AsterixMitic@users.noreply.github.com>
2021-11-16 16:26:09 +0100

    Login i register funkcionalni i povezani sa firebase

--- app/src/main/java/com/example/tourme/Login.java
+++ app/src/main/java/com/example/tourme/Login.java
@@ -1,14 +1,66 @@
 package com.example.tourme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
+
+    EditText mEmail, mPassword;
+    Button login_dugme;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.email_login);
+        mPassword = findViewById(R.id.password_login);
+        login_dugme = findViewById(R.id.login_dugme);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        login_dugme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Unesite email");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Unesite sifru");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Login.this,"Uspesno",Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            Toast.makeText(Login.this,"Greska" + task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tourme/Register.java
+++ app/src/main/java/com/example/tourme/Register.java
@@ -1,14 +1,79 @@
 package com.example.tourme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+
+    EditText mEmail, mPassword, mConfirmPassword;
+    Button register_dugme;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        mConfirmPassword = findViewById(R.id.confirm_password);
+
+        register_dugme = findViewById(R.id.register_dugme);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        register_dugme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                String confirm_password = mConfirmPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Unesite email");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Unesite sifru");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(confirm_password)){
+                    mConfirmPassword.setError("Potvrdite sifru");
+                    return;
+                }
+
+                if(!TextUtils.equals(confirm_password,password)){
+                    mConfirmPassword.setError("Sifre nisu iste!");
+                    return;
+                }
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register.this,"Uspesno",Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            Toast.makeText(Register.this,"Greska" + task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
