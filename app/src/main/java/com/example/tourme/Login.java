bb3bdccf99e88009b2b47a3ca4ddf4095bd900df
Luka Kovacevic <luka.kovacevic03@gmail.com>
2021-11-24 23:19:01 +0100

    dodao sam login i register sa username

--- app/src/main/java/com/example/tourme/Login.java
+++ app/src/main/java/com/example/tourme/Login.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,12 +15,85 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Login extends AppCompatActivity {
 
     EditText mEmail, mPassword;
     Button login_dugme;
     FirebaseAuth fAuth;
+
+    boolean didFindError = false;
+
+    private DatabaseReference mDatabase;
+
+    String patternForEmail = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+
+    void setEmailError(String errorText){
+        mEmail.setError(errorText);
+        didFindError = true;
+    }
+
+    void setPasswordError(String errorText){
+        mPassword.setError(errorText);
+        didFindError = true;
+    }
+
+    void finishSigningIn(String email, String password){
+        fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(Login.this,"Uspesno",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
+                    Log.e("gf", errorCode);
+                    if(errorCode.equals("ERROR_USER_NOT_FOUND")){
+                        setEmailError("Ne postoji nalog sa ovim email-om");
+                    }else if(errorCode.equals("ERROR_WRONG_PASSWORD")){
+                        setPasswordError("Pogresna sifra");
+                    }else{
+                        Toast.makeText(Login.this,"Greska" + task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
+
+    void startSigningIn(String email, String password){
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(patternForEmail);
+        java.util.regex.Matcher m = p.matcher(email);
+
+
+        if(!m.matches()) {
+            //nije email nego je username
+            String username = email;
+            mDatabase.child("users").child(username).child("email").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if (!task.isSuccessful()) {
+                        Log.e("Firebase", "Error getting data", task.getException());
+                    }
+                    else {
+                        String emailFromDatabase = String.valueOf(task.getResult().getValue());
+                        if(!emailFromDatabase.equals("null")){
+                            finishSigningIn(emailFromDatabase, password);
+                        }else{
+                            setEmailError("Ne postoji ovakvo korisnicko ime");
+                        }
+                    }
+                }
+            });
+        }else{
+            finishSigningIn(email, password);
+        }
+
+        //finishSigningIn(email, password);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,33 +106,25 @@ public class Login extends AppCompatActivity {
 
         fAuth = FirebaseAuth.getInstance();
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         login_dugme.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                didFindError = false;
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
 
                 if(TextUtils.isEmpty(email)){
-                    mEmail.setError("Unesite email");
-                    return;
+                    setEmailError("Unesite email");
                 }
 
                 if(TextUtils.isEmpty(password)){
-                    mPassword.setError("Unesite sifru");
-                    return;
+                    setPasswordError("Unesite sifru");
                 }
-
-                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(Login.this,"Uspesno",Toast.LENGTH_LONG).show();
-                        }
-                        else{
-                            Toast.makeText(Login.this,"Greska" + task.getException().getMessage(),Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
+                if(!didFindError){
+                    startSigningIn(email, password);
+                }
 
             }
         });
--- app/src/main/java/com/example/tourme/Register.java
+++ app/src/main/java/com/example/tourme/Register.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +21,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 
 import com.google.firebase.database.FirebaseDatabase;
@@ -39,6 +41,7 @@ public class Register extends AppCompatActivity {
     //TextView passwordNeedText;
 
     String patternForEmail = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+    String patternForUsername = "^[a-zA-Z0-9]+$";
 
     boolean didFindError = false;
 
@@ -62,18 +65,16 @@ public class Register extends AppCompatActivity {
         didFindError = true;
     }
 
-    void createAccount(String AccountEmail, String AccountPassword){
+    void finishCreatingAccount(String AccountEmail, String AccountPassword) {
         fAuth.createUserWithEmailAndPassword(AccountEmail,AccountPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-                    fAuth.getCurrentUser().getUid();
                     Toast.makeText(Register.this,"Uspesno ste kreirali nalog",Toast.LENGTH_LONG).show();
+
                     String userId = fAuth.getCurrentUser().getUid();
                     User user = new User(userId, AccountEmail, username, "default");
-                    FirebaseDatabase.getInstance().getReference("users").child(userId).setValue(user);
-
-
+                    mDatabase.child("users").child(username).setValue(user);
                 }
                 else{
                     String errorCode = ((FirebaseAuthException) task.getException()).getErrorCode();
@@ -82,6 +83,25 @@ public class Register extends AppCompatActivity {
                         setEmailError("Vec postoji nalog sa ovim Email-om");
                     }else {
                         Toast.makeText(Register.this, task.getException().toString(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
+    }
+    void startCreatingAccount(String AccountEmail, String AccountPassword){
+        mDatabase.child("users").child(username).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("Firebase", "Error getting data", task.getException());
+                }
+                else {
+                    String dataFromDatabase = String.valueOf(task.getResult().getValue());
+                    if(!dataFromDatabase.equals("null")){
+                        setUsernameError("Vec postoji nalog sa ovim korisnickim imenom");
+                    }else{
+                        finishCreatingAccount(AccountEmail, AccountPassword);
                     }
                 }
             }
@@ -104,17 +124,78 @@ public class Register extends AppCompatActivity {
 
         fAuth = FirebaseAuth.getInstance();
 
-        //luka ovo su nove izmene
-        //FirebaseFirestore db = FirebaseFirestore.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = mEmail.getText().toString().trim();
+                username = mUserName.getText().toString().trim();
+                password = mPassword.getText().toString().trim();
+                confirm_password = mConfirmPassword.getText().toString().trim();
+
+                didFindError = false;
+
+                if(TextUtils.isEmpty(email)){
+                    setEmailError("Unesite Email");
+                }else{
+                    java.util.regex.Pattern p = java.util.regex.Pattern.compile(patternForEmail);
+                    java.util.regex.Matcher m = p.matcher(email);
+
+                    if(!m.matches()) {
+                        setEmailError("Email nije pravog formata");
+                    }
+                }
+
+                if(TextUtils.isEmpty(username)){
+                    setUsernameError("Unesti korisnicko ime");
+                }else{
+                    java.util.regex.Pattern p = java.util.regex.Pattern.compile(patternForUsername);
+                    java.util.regex.Matcher m = p.matcher(username);
+
+                    if(!m.matches()) {
+                        setUsernameError("Korisnicko ime mora da se sastoji samo od slova i brojeva");
+                    }
+                }
+
+                int len = password.length();
+                if(len >= 1 && len < 6){
+                    setPasswordError("Sifra mora imati vise 6 - 12 karaktera");
+                    mConfirmPassword.setError(null);
+                }else{
+                    if(TextUtils.isEmpty(password)) {
+                        setPasswordError("Unesite Sifru");
+                    }
+
+                    if(TextUtils.isEmpty(confirm_password)){
+                        setConfirmPassword("Potvrdite Sifru");
+                    }else if(!TextUtils.equals(confirm_password,password)){
+                        setConfirmPassword("Sifre nisu iste");
+                    }
+                }
 
 
-        //FirebaseDatabase.getInstance()
+                if(!didFindError){
+                    startCreatingAccount(email, password);
+                }
+
+            }
+        });
+
+    }
+}
+
+//luka ovo su nove izmene
+//FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+//FirebaseDatabase.getInstance()
 //        HashMap<String, String> hashMap = new HashMap<>();
 //        hashMap.put("id", "1");
 //        hashMap.put("username","yutopk");
 //        hashMap.put("imageurl","default");
 
-        //FirebaseDatabase.getInstance().getReference("korisnik").child("1").setValue(hashMap);
+//FirebaseDatabase.getInstance().getReference("korisnik").child("1").setValue(hashMap);
         /*
 
         OVO CE NAM KASNIJE TREBA ZA CUVANEJ SLIKE
@@ -144,55 +225,3 @@ public class Register extends AppCompatActivity {
         });
         *
         * */
-
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                email = mEmail.getText().toString().trim();
-                username = mUserName.getText().toString().trim();
-                password = mPassword.getText().toString().trim();
-                confirm_password = mConfirmPassword.getText().toString().trim();
-
-                didFindError = false;
-
-                if(TextUtils.isEmpty(email)){
-                    setEmailError("Unesite Email");
-                }else{
-                    java.util.regex.Pattern p = java.util.regex.Pattern.compile(patternForEmail);
-                    java.util.regex.Matcher m = p.matcher(email);
-
-                    if(!m.matches()) {
-                        setEmailError("Email nije pravog formata");
-                    }
-                }
-
-                if(TextUtils.isEmpty(username)){
-                    setUsernameError("Unesti korisnicko ime");
-                }
-
-                int len = password.length();
-                if(len >= 1 && len < 6){
-                    setPasswordError("Sifra mora imati vise 6 - 12 karaktera");
-                    mConfirmPassword.setError(null);
-                }else{
-                    if(TextUtils.isEmpty(password)) {
-                        setPasswordError("Unesite Sifru");
-                    }
-
-                    if(TextUtils.isEmpty(confirm_password)){
-                        setConfirmPassword("Potvrdite Sifru");
-                    }else if(!TextUtils.equals(confirm_password,password)){
-                        setConfirmPassword("Sifre nisu iste");
-                    }
-                }
-
-
-                if(!didFindError){
-                    createAccount(email, password);
-                }
-
-            }
-        });
-
-    }
-}
