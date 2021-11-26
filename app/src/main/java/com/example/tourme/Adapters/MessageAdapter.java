aaf42be9fea3a55218c3dc7b6cb0574160ebc7c2
AsterixMitic <58214631+AsterixMitic@users.noreply.github.com>
2021-11-26 11:06:09 +0100

    Vraceno na radnu verziju

---
+++ app/src/main/java/com/example/tourme/Adapters/MessageAdapter.java
@@ -0,0 +1,87 @@
+package com.example.tourme.Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.tourme.MessageActivity;
+import com.example.tourme.Model.Chat;
+import com.example.tourme.Model.User;
+import com.example.tourme.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
+    public static final int MSG_TYPE_LEFT = 0;
+    public static final int MSG_TYPE_RIGHT = 1;
+
+    private Context mContext;
+    private List<Chat> mChat;
+    private String imageurl;
+
+    FirebaseUser fUser;
+
+    public MessageAdapter(Context mContext, List<Chat> mChat, String imageurl){
+        this.mContext=mContext;
+        this.mChat=mChat;
+        this.imageurl = imageurl;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.users_item, parent, false);
+        return new MessageAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mChat.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView show_message;
+        public ImageView profile_image;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            show_message = itemView.findViewById(R.id.show_message);
+            profile_image = itemView.findViewById(R.id.profile_image);
+
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        fUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(mChat.get(position).getSender().equals(fUser.getUid())){
+            return MSG_TYPE_RIGHT;
+        }
+        else{
+            return MSG_TYPE_LEFT;
+        }
+    }
+}
