ec79dae03eee816e36779f9b537a205a71ca36a7
AsterixMitic <58214631+AsterixMitic@users.noreply.github.com>
2022-01-04 11:34:05 +0100

    FCM token, dodati gradovi

--- app/src/main/java/com/example/tourme/PushNotificationService.java
+++ app/src/main/java/com/example/tourme/PushNotificationService.java
@@ -1,19 +1,33 @@
 package com.example.tourme;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class PushNotificationService extends FirebaseMessagingService {
-
-    private static final String TAG = PushNotificationService.class.getSimpleName();
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.e("newToken", s);
+        getSharedPreferences("_", MODE_PRIVATE).edit().putString("fb", s).apply();
+    }
 
     @Override
-    public void onNewToken(@NonNull String s) {
-        super.onNewToken(s);
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
 
-        Log.d(TAG, "Token: " + s);
+    public static String getToken(Context context) {
+        return context.getSharedPreferences("_", MODE_PRIVATE).getString("fb", "empty");
     }
 }
